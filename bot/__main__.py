5651d9aeb7ecfb6fad8efe26e0230e5f7e5e2fd9
yuno74 <65702879+yuno74@users.noreply.github.com>
2021-07-21 18:16:27 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -19,7 +19,7 @@ from bot.helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper import button_build
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete, usage, mediainfo, count, anime
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete, usage, mediainfo, count
 now=datetime.now(pytz.timezone(f'{TIMEZONE}'))
 
 
