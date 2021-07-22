cd42d1725aa6824654690a294feb08e9df173446
yuno74 <65702879+yuno74@users.noreply.github.com>
2021-07-22 20:28:34 +0800

    Add files via upload

---
+++ bot/localisation.py
@@ -0,0 +1,14 @@
+class Localisation:
+    STATUS_UPLOADING = "📤 ᴜᴘʟᴏᴀᴅɪɴɢ 📤"
+   
+    STATUS_DOWNLOADING = "📥 ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ 📥"
+    
+    STATUS_CLONING = "♻ ️ᴄʟᴏɴɪɴɢ"
+    
+    STATUS_WAITING = "📄 ǫᴜᴇǫᴇᴅ"
+    
+    STATUS_FAILED = "🚫 ғᴀɪʟᴇᴅ"
+    
+    STATUS_ARCHIVING = "🔐 ᴀʀᴄʜɪᴠɪɴɢ"
+    
+    STATUS_EXTRACTING = "📂 ᴇxᴛʀᴀᴄᴛɪɴɢ"
